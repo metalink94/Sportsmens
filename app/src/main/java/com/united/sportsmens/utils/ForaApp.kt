0caa5fa37c6a491e4bb3b9dbcfca9bd00d0bd082
Denis <denis.novikov@akbarsdigital.ru>
2019-08-24 10:57:53 +0300

    add app

--- app/src/main/java/com/united/sportsmens/utils/ForaApp.kt
+++ app/src/main/java/com/united/sportsmens/utils/ForaApp.kt
@@ -1,21 +1,14 @@
 package com.united.sportsmens.utils
 
 import androidx.multidex.MultiDexApplication
-import com.facebook.FacebookSdk
-import com.facebook.LoggingBehavior
-import com.facebook.appevents.AppEventsLogger
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
 class ForaApp: MultiDexApplication() {
 
-    lateinit var logger: AppEventsLogger
-
     override fun onCreate() {
         FirebaseApp.initializeApp(this)
         super.onCreate()
         FirebaseMessaging.getInstance().isAutoInitEnabled = true
-        FacebookSdk.addLoggingBehavior(LoggingBehavior.APP_EVENTS)
-        logger = AppEventsLogger.newLogger(this)
     }
 }
